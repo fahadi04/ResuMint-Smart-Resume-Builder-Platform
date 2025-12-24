8043dfff1155e58b468addf40e6841da0fa7cbf2
fahadi04 <fahadmasrur786@gmail.com>
2025-12-24 19:12:47 +0530

    Complete Authentication

---
+++ resumebuilder/src/main/java/com/project/resumebuilder/Utils/AppConstants.java
@@ -0,0 +1,8 @@
+package com.project.resumebuilder.Utils;
+
+public class AppConstants {
+    public static final String AUTH_CONTROLLER = "/api/auth";
+    public static final String REGISTER = "/register";
+    public static final String VERIFY_EMAIL = "/verify-email";
+    public static final String UPLOAD_IMAGE="/upload-image";
+}
---
+++ resumebuilder/src/main/java/com/project/resumebuilder/controller/AuthController.java
@@ -0,0 +1,63 @@
+package com.project.resumebuilder.controller;
+
+import com.project.resumebuilder.dto.AuthResponse;
+import com.project.resumebuilder.dto.LoginRequest;
+import com.project.resumebuilder.dto.RegisterRequest;
+import com.project.resumebuilder.service.AuthService;
+import com.project.resumebuilder.service.FileUploadService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.project.resumebuilder.Utils.AppConstants.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping(AUTH_CONTROLLER)
+public class AuthController {
+
+    private final AuthService authService;
+    private final FileUploadService fileUploadService;
+
+    @PostMapping(REGISTER)
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
+        log.info("Inside AuthController - register():{}", request);
+        AuthResponse response = authService.register(request);
+        log.info("Response from service: {}", response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+
+    @GetMapping(VERIFY_EMAIL)
+    public ResponseEntity<?> verifyEmail(@RequestParam String token) {
+        log.info("Inside AuthController - verifyEmail():{}", token);
+        authService.verifyEmail(token);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "Email verified successfully...!!"));
+    }
+
+    @PostMapping(UPLOAD_IMAGE)
+    public ResponseEntity<?> uploadImage(@RequestPart("image") MultipartFile file) throws IOException {
+        log.info("Inside AuthController - uploadImage()");
+        Map<String, String> response = fileUploadService.uploadSingleImage(file);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/fvalidate")
+    public String testValidationToken() {
+        return "Token Validation is working";
+    }
+}
