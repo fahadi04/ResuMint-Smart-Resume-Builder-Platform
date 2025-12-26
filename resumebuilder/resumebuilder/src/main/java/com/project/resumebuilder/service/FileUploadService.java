17ca717f7a25be47c520dd5719bd6c267e3f2831
fahadi04 <fahadmasrur786@gmail.com>
2025-12-27 03:12:00 +0530

    Razorpay Integratin done

--- resumebuilder/resumebuilder/src/main/java/com/project/resumebuilder/service/FileUploadService.java
+++ resumebuilder/resumebuilder/src/main/java/com/project/resumebuilder/service/FileUploadService.java
@@ -7,6 +7,7 @@ import com.project.resumebuilder.dto.AuthResponse;
 import com.project.resumebuilder.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
